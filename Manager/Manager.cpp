d84c9ec6017f633b519686a2304e2ce8c4400143
KorolevIvanMi <Ivankorol1512@yandex.ru>
2026-07-23 21:51:03 +0300

    change logic

--- Manager/Manager.cpp
+++ Manager/Manager.cpp
@@ -17,23 +17,29 @@ manager::Manager::Manager(){
 manager::Manager::Manager(std::string journalName, manager::Level defaultLevel): journalName{journalName} , defaultLevel{defaultLevel}{}
 
 // Превращает текст сообщения и уровень важности в строку для записи в журнал + добавляет дату записи
+// также проверяет соотвествие уровня задач
+// возврат пустой строки нужен для того, чтобы при реализации в приложении можно было отследить некорректный уровень
 std::string manager::Manager::ConvertRow(std::string text,manager::Level level){
-    // необходимые переменные
-    std::string result = " ";
-    time_t t= time(NULL);
-    int lvl = static_cast<int>(level); // получение значения из перечисления, так как хранить 1 символ лучше чем несколько, то храним номер важности
 
-    // строка-поток для перевода времени в строку
-    std::stringstream ss;
-    ss << std::put_time(std::localtime(&t), "%Y-%m-%d-%H:%M:%S");
-    
-    // формирование и возврат строки
-    result.append(std::to_string(lvl));
-    result.append(" ");
-    result.append(ss.str());
-    result.append(" ");
-    result.append(text);
-    return result;
+    if (static_cast<int>(level) < static_cast<int>(defaultLevel)) { 
+        // необходимые переменные
+        std::string result = " ";
+        time_t t= time(NULL);
+        int lvl = static_cast<int>(level); // получение значения из перечисления, так как хранить 1 символ лучше чем несколько, то храним номер важности
+
+        // строка-поток для перевода времени в строку
+        std::stringstream ss;
+        ss << std::put_time(std::localtime(&t), "%Y-%m-%d-%H:%M:%S");
+        
+        // формирование и возврат строки
+        result.append(std::to_string(lvl));
+        result.append(" ");
+        result.append(ss.str());
+        result.append(" ");
+        result.append(text);
+        return result;
+    }
+    return ""; //если важность задачи по умолчанию больше переданного, то возвращаем пустую строку
 }
 
 // записывает переданную строку в файл . Возвращает true есил всё получилось и flase, если возникли проблемы
@@ -51,9 +57,14 @@ bool manager::Manager::WriteToJournal(std::string text){
 
 // обрабатывает и записывает данные в файл . По сути является объединением ConvertRow и WriteToJournal
 // нужен для тех случаев, когда разработчику хочется вызвать один метод вместо двух. 
-// только в таком случае при многопоточной работе уже внутри приложения нельзя пустить запись в файл под mutex
+// только в таком случае при многопоточной работе уже внутри приложения нельзя пустить запись в файл под mutex и если и пускать, то и обработку текста сообщения
+// так что в зависимости от требования приложения можно использовать или отдельную реализацию или объединнную
 bool manager::Manager::Write(std::string text,manager::Level level){
     std::string line = ConvertRow(text,  level);
+    if (line == ""){
+        return false;
+    }
     if (!WriteToJournal(line)) return false;
     return true;
 }
+
--- Manager/Manager.h
+++ Manager/Manager.h
@@ -34,7 +34,6 @@ namespace manager {
         manager::Level defaultLevel;
 
         // Скрытые технические методы
-        
         void parseData();
     };
 }
