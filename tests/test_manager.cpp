fefde39f8d06e16c5de723739d834f4bfe9dfe6f
KorolevIvanMi <Ivankorol1512@yandex.ru>
2026-07-25 23:38:23 +0300

    add writeToJournal Test

--- tests/test_manager.cpp
+++ tests/test_manager.cpp
@@ -87,3 +87,48 @@ bool test_ConvertRow_LevelBelowDefault(){
 }
 
 // ТЕСТЫ WRITE TO JOURNAL
+// тест на создание новой записи
+bool test_WriteToJournal_Success(){
+    std::string filename = CreateTempFilename();
+    manager::Manager mng (filename, manager::Level::MEDIUM);
+
+    std::string value = "Test message";
+    std::string message = mng.ConvertRow(value, manager::Level::IMPORTANT);
+    bool result = mng.WriteToJournal(message);
+
+    TEST_ASSERT(result);
+    TEST_ASSERT(std::filesystem::exists(filename));
+    
+    std::ifstream file(filename);
+    std::string content;
+    std::getline(file, content);
+    TEST_ASSERT(content == message);
+    
+    Cleanup(filename);
+    return true;
+}
+
+bool test_WriteToJournal_AppendMode() {
+    std::string filename = CreateTempFilename();
+    manager::Manager mng(filename, manager::Level::MEDIUM);
+    
+    std::string value = "First";
+    std::string msg1 = mng.ConvertRow(value, manager::Level::MEDIUM);
+    value = "Second";
+    std::string msg2 = mng.ConvertRow(value, manager::Level::MEDIUM);
+    
+    mng.WriteToJournal(msg1);
+    mng.WriteToJournal(msg2);
+    
+    // Проверяем, что обе записи есть
+    std::ifstream file(filename);
+    std::string line1, line2;
+    std::getline(file, line1);
+    std::getline(file, line2);
+    
+    TEST_ASSERT(line1 == msg1);
+    TEST_ASSERT(line2 == msg2);
+    
+    Cleanup(filename);
+    return true;
+}
